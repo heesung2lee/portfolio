803630758e24aa1037a957fd623ed3bccbe9ef0c
heesung2lee <heesung2lee@gmail.com>
2023-01-31 13:57:20 -0800

    centering resume section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -292,12 +292,12 @@ export default function Home({ articles }) {
       </Container>
       <Photos />
       <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-            {/* {articles.map((article) => (
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-1">
+          {/* <div className="flex flex-col gap-16">
+            {articles.map((article) => (
               <Article key={article.slug} article={article} />
-            ))} */}
-          </div>
+            ))}
+          </div> */}
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             {/* <Newsletter /> */}
             <Resume />
